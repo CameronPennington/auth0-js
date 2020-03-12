7c825f95ed78e153184453c3716fa0eb764489ab
CameronPennington <cameron.pennington@gmail.com>
2020-03-12 09:43:46 -0400

    auth0 authentication added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
 // define the Express app
 const app = express();
@@ -41,21 +43,36 @@ app.get("/:id", (req, res) => {
 	res.send(question[0]);
 });
 
+const checkJwt = jwt({
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://<YOUR_AUTH0_DOMAIN>/.well-known/jwks.json`
+	}),
+
+	// Validate the audience and the issuer.
+	audience: "7dXGhu0vQNk6NUorgMW2i7t4e4eme5Eb",
+	issuer: `https://penncoffee.auth0.com/`,
+	algorithms: ["RS256"]
+});
+
 // insert a new question
-app.post("/", (req, res) => {
+app.post("/", checkJwt, (req, res) => {
 	const { title, description } = req.body;
 	const newQuestion = {
 		id: questions.length + 1,
 		title,
 		description,
-		answers: []
+		answers: [],
+		author: req.user.name
 	};
 	questions.push(newQuestion);
 	res.status(200).send();
 });
 
 // insert a new answer to a question
-app.post("/answer/:id", (req, res) => {
+app.post("/answer/:id", checkJwt, (req, res) => {
 	const { answer } = req.body;
 
 	const question = questions.filter(q => q.id === parseInt(req.params.id));
@@ -63,7 +80,8 @@ app.post("/answer/:id", (req, res) => {
 	if (question.length === 0) return res.status(404).send();
 
 	question[0].answers.push({
-		answer
+		answer,
+		author: req.user.name
 	});
 
 	res.status(200).send();
